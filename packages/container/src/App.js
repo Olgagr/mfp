c1729a592e9dd70a4e3d49d014db71e41fa1b844
Olga Grabek <olga.grabek@twipemobile.com>
2021-08-28 10:29:11 +0200

    Initial commit

---
+++ packages/container/src/App.js
@@ -0,0 +1,9 @@
+import React from "react";
+import MarketingApp from "./components/MarketingApp";
+
+export default () => {
+  return <div id="container">
+    <h1>container</h1>
+    <MarketingApp></MarketingApp>
+  </div>
+};
